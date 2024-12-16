4d483fec10a752c175bee0e342596becdb272964
isabelfabio <isabelffabio@gmail.com>
2024-12-15 20:17:45 -0500

    add workflows, edit fib module, add testing

--- factorial.py
+++
@@ -1,7 +0,0 @@
-def factorial (n):
-    if n <= 1:
-        return n
-    else:
-        return factorial(n-1) + factorial(n-2)
-
-print(factorial(23))
---
+++ testFibonacci.py
@@ -0,0 +1,11 @@
+import pytest
+from fibonacci import fibonacci
+
+def test_fibonacci():
+    # Test the first few Fibonacci numbers
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
