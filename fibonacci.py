7b3ff7564cc9591cb7c1ee136c6bfa6c81e2baad
isabelfabio <isabelffabio@gmail.com>
2024-12-16 21:14:37 -0500

    lgtm, push to main

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
+def fibonacci(n): # okay this seems to work just fine, no real fixing needed
     print(f"you are printing the {n}th term of the fibonacci sequence!")
     if n <= 1:
         return n
